590a705120518f24e8690e49bb6e640b2e63e8ee
LHY <lhy30919@naver.com>
2025-03-04 11:36:12 +0900

    2025.03.04 수정

---
+++ app/src/main/java/com/capstone/Algan/AlamFragment.kt
@@ -0,0 +1,16 @@
+package com.capstone.Algan
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class AlamFragment : Fragment() {
+
+
+}
